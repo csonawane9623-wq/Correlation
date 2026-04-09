09fbaaa2a9e8a0ae49f1b140f8156bd2d73344c3
csonawane9623-wq <csonawane9623@gmail.com>
2026-04-09 12:48:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -152,7 +152,7 @@ def get_top_pairs(df, funding):
 # TELEGRAM
 # =========================
 def send_telegram(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     requests.post(url, data={
         "chat_id": TELEGRAM_CHAT_ID,
